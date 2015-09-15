87d2396af5fe563b647cdceb951bfd74b59ca409
gsan13 <gsandita.cs@gmail.com>
2015-09-15 12:19:31 +0700

    Refactor home page view to use a template

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-# Create your views here.
 def home_page(request):
-	return HttpResponse('<html><title>Homepage Pribadi</title>Givana Sandita, 1206208076</html>')
+	return render(request, 'home.html')
