954f1bee5c7e3d9b76047c97d0cac7d18eb84c2a
gsan13 <gsandita.cs@gmail.com>
2015-09-17 08:31:27 +0700

    komentar otomatis dengan unit test ok

--- lists/tests.py
+++ lists/tests.py
@@ -12,11 +12,11 @@ class HomePageTest(TestCase):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
 
-	#def test_home_page_returns_correct_html(self):
-		#request = HttpRequest()
-		#response = home_page(request)
-		#expected_html = render_to_string('home.html')
-		#self.assertEqual(response.content.decode(), expected_html)
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		expected_html = render_to_string('home.html',{'comment':"yey, waktunya berlibur"})
+		self.assertEqual(response.content.decode(), expected_html)
 
 	def test_home_page_can_save_a_POST_request(self):
 		request = HttpRequest()
